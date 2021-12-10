6d41f5de79e44fd09d4041d0bff17a0efd6bfca5
severeduck <92829782+severeduck@users.noreply.github.com>
2021-12-10 22:29:17 +0100

    Refactor 🚜

--- Advent/Sources/Day10/SyntaxScoring.swift
+++ Advent/Sources/Day10/SyntaxScoring.swift
@@ -8,77 +8,49 @@ public struct SyntaxScoring {
         let openingBraces = ["(", "[", "{", "<"]
         let closingBraces = [")", "]", "}", ">"]
         let bracesPairs = [("(", ")"), ("[", "]"), ("{", "}"), ("<", ">")]
-        let errorValuesTable = [")": 3, "]": 57, "}": 1197, ">": 25137]
         let scoreCalculationRules = [")": 1, "]": 2, "}": 3, ">": 4]
-        var errorsFound = [String]()
-        var completingLines = [String]()
         var completingLineScores = [Int]()
 
         input.forEach {
             var stack = [String]()
-            var errorSymbol: String?
+            var isLineIncorrect = false
             var index = 0
             
             repeat {
                 let stringIndex = $0.index($0.startIndex, offsetBy: index)
                 let symbol = String($0[stringIndex])
-//                print("symbol[\(index)] == \(symbol)")
                 index += 1
-                
-                let testOpposite = findOppositeBrace(for: symbol, in: openingBraces, bracesPairs: bracesPairs)
-                print("sybmol: \(symbol); testOpposite: \(testOpposite)")
-                
+                                
                 if openingBraces.contains(symbol) {
                     stack.append(symbol)
-//                    print("\(stack) <= \(symbol) == \(stack)")
                     continue
                 }
                 
                 guard let lastFromStack = stack.popLast() else {
                     stack.append(symbol)
-//                    print("\(stack) <= \(symbol) == \(stack)")
                     continue
                 }
                 
-                guard let indexToCheck = closingBraces.firstIndex(of: symbol) else {
-                    errorSymbol = symbol
-//                    print("errorSymbol = \(symbol)")
-                    break
-                }
-                
-                let pairToCheckAgainst = bracesPairs[indexToCheck]
-                
-//                print("lastFromStack: \(lastFromStack) == pairToCheckAgainst.0: \(pairToCheckAgainst.0)")
                 guard lastFromStack == findOppositeBrace(for: symbol, in: closingBraces, bracesPairs: bracesPairs) else {
-                    errorSymbol = symbol
-//                    print("errorSymbol = \(symbol)")
+                    isLineIncorrect = true
                     break
                 }
-                
-//                _ = stack.popLast()
-//                print("\(stack) >= last == \(stack)")
-                
-            } while index < $0.count && errorSymbol == nil
+            } while index < $0.count && !isLineIncorrect
             
-            if let errorSymbol = errorSymbol {
-                errorsFound.append(errorSymbol)
-            } else {
-                let incompleteLine = stack.reduce("", +)
-                let completingLine = incompleteLine
-                    .reversed()
-                    .compactMap { findOppositeBrace(for: String($0), in: openingBraces, bracesPairs: bracesPairs) }
-                    .joined()
-                completingLines.append(completingLine)
-                
-                var score = 0
-                completingLine.forEach {
-                    let symbol = String($0)
-                    score *= 5
-                    score += scoreCalculationRules[symbol]!
-                }
-                
-                completingLineScores.append(score)
+            if isLineIncorrect { return }
+
+            let incompleteLine = stack.reduce("", +)
+            let completingLine = incompleteLine
+                .reversed()
+                .compactMap { findOppositeBrace(for: String($0), in: openingBraces, bracesPairs: bracesPairs) }
+                .joined()
+            
+            var score = 0
+            completingLine.forEach {
+                score *= 5
+                score += scoreCalculationRules[String($0)]!
             }
+            completingLineScores.append(score)
         }
         
         return completingLineScores.sorted(by: <)[(completingLineScores.count / 2)]
@@ -89,6 +61,7 @@ public struct SyntaxScoring {
         in symbols: [String],
         bracesPairs: [(String, String)]
     ) -> String? {
+        
         guard let indexToCheck = symbols.firstIndex(of: symbol) else {
             return nil
         }
@@ -100,6 +73,3 @@ public struct SyntaxScoring {
             : pairToCheckAgainst.0
     }
 }
-
-// [({([[{{ }}]])})]
-// [({([[{{
